760fb60ffb4fe1edfb38eda70e3b6ad4878d2208
tesfayeKebebe <42234406+tesfayeKebebe@users.noreply.github.com>
2022-07-05 17:51:32 +0300

    transversal tree

--- dotnetchallenge/src/Trees Challenges/BinaryTree.cs
+++ dotnetchallenge/src/Trees Challenges/BinaryTree.cs
@@ -1,6 +1,20 @@
 ﻿using System;
+using System.Collections.Generic;
+
 namespace dotnetchallenge.TreesChallenges
 {
+    public class TreeNode
+    {
+        public int val;
+        public TreeNode left;
+        public TreeNode right;
+        public TreeNode(int val = 0, TreeNode left = null, TreeNode right = null)
+        {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
     public static class BinaryTree
     {
         //Binary Search Algorithm (Finding Target in a Sorted List)
@@ -42,5 +56,44 @@ namespace dotnetchallenge.TreesChallenges
             }
             return retValue;
         }
+        //Definition for a binary tree node.
+
+//        Given the root of a binary tree, return the level order traversal of its nodes' values. (i.e., from left to right, level by level).
+
+//Example 1:
+//    3
+///9     20
+// /    15 7
+
+//Input: root = [3,9,20,null,null,15,7]
+//        Output: [[3],[9,20],[15,7]]
+
+            public static IList<IList<int>> LevelOrder(TreeNode root)
+            {
+                IList<IList<int>> result = new List<IList<int>>();
+                if (root != null)
+                {
+                    AddValue(root, 0);
+                }
+                return result;
+
+                void AddValue(TreeNode node, int level)
+                {
+                    if (result.Count == level)
+                    {
+                        result.Add(new List<int>());
+                    }
+                    result[level].Add(node.val);
+                    if (node.left != null)
+                    {
+                        AddValue(node.left, level + 1);
+                    }
+                    if (node.right != null)
+                    {
+                        AddValue(node.right, level + 1);
+                    }
+                }
+            }
+        
     }
 }
